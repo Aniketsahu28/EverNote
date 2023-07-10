e14e9adb7b643d8f8c3c07189bffdac8f3e99543
Aniket <aniketsahu1711@gmail.com>
2023-07-10 21:57:38 +0530

    added host in env

--- backend/index.js
+++ backend/index.js
@@ -10,10 +10,10 @@ app.use(cors());
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-
+const host = process.env.BASE_URL
 //Available routes
-app.use("/api/auth", require("./routes/auth"));
-app.use("/api/notes", require("./routes/notes"));
+app.use(`${host}/api/auth`, require("./routes/auth"));
+app.use(`${host}/api/notes`, require("./routes/notes"));
 
 app.listen(port, () => {
   console.log(`everNote backend listening at http://localhost:${port}`);
